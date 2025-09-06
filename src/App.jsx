3ddae07f1e782012c5b94cb200afc56486015e75
Guille <guibear0@gmail.com>
2025-09-06 16:56:59 +0200

    /Home

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
