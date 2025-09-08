00ae3864f389c870e60b8ad1726bc9d7dad406c6
Guille <guibear0@gmail.com>
2025-09-08 16:58:02 +0200

    ImportJS

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import emailjs from "emailjs-com";
+import emailjs from '@emailjs/browser';
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState("");
